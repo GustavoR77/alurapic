0fcb89f6b5db4f08808b571dae76718385efa63b
Gustavo Ribeiro <52572546+GustavoR77@users.noreply.github.com>
2020-12-10 15:08:23 -0300

    rotas configuradas 1.1
    rotas com mais configurações, dessa vez com a adição de uma página de não existente para rotas não configuradas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+import { ErrorsModule } from './errors/errors.module';
 import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
 
@@ -13,6 +14,7 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule,
     PhotosModule,
     AppRoutingModule,
+    ErrorsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
